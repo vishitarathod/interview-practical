b5fe1bcfe332a3de3e7fa7e8189d7cff65933f49
vishita.r <vishita.r@simformsolutions.com>
2023-07-15 13:37:39 +0530

    [frontend]:added post module with create and list routing

--- practical-frontend/src/app/app-routing.module.ts
+++ practical-frontend/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found/not-found.component';
 
-const routes: Routes = [{
-  path:'auth',
-  loadChildren:()=> import('../app/auth/auth.module').then((m)=>m.AuthModule)
-}];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'post'
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('../app/auth/auth.module').then((m) => m.AuthModule)
+  },
+  {
+    path: 'post',
+    loadChildren: () => import('../app/posts/posts.module').then((m) => m.PostsModule)
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
